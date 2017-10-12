cabca8ea7d7372754806180b82142e89ba97ea40
ryought <ryonakabayashi@gmail.com>
2017-10-12 22:23:06 +0900

    forward and backward completed

--- DP/prob.py
+++ DP/prob.py
@@ -39,7 +39,7 @@ def add(logB, logC):
     logA = m + log(exp(logB-m) + exp(logC-m))
     return logA
 
-def forward(x=len(S1), y=len(S2)):
+def forward(S1, S2, x, y):
     # 配列宣言
     fM  = [['null' for _ in range(len(S2)+1)] for _ in range(len(S1)+1)]
     fx  = [['null' for _ in range(len(S2)+1)] for _ in range(len(S1)+1)]
@@ -82,23 +82,48 @@ def forward(x=len(S1), y=len(S2)):
             # fy(i,j) = fm(i,j-1)exp(-d/t) + fy(i,j-1)exp(-e/t)
             fy[i][j] = add(fM[i][j-1] - d/T, fy[i][j-1] - e/T)
 
+    for i in range(len(S1)+1):
+        for j in range(len(S2)+1):
             # f(i,j) = fm(i,j) + fx(i,j) + fy(i,j)
             f[i][j] = fM[i][j] + fx[i][j] + fy[i][j]
 
     # 点数の確認
-    print('Z:', f[len(S1)][len(S2)], exp(f[len(S1)][len(S2)]))
     return f[x][y]
 
+def backward(S1, S2, x, y):
+    # 文字列自体を逆順にforwardアルゴリズムを回せば、backwardと同じになるはず
+    R1 = S1[::-1]
+    R2 = S2[::-1]
+    return forward(R1, R2, len(R1)-x, len(R2)-y)
 
+
+def score_probability():
+    logZ = forward(S1, S2, len(S1), len(S2))
+    # P = exp(S/T) / Z より、 logP = S/T - logZ
+    Score = -1  # NWG.pyの実行結果
+    logP = Score/T - logZ
+    print('Z:', logZ, exp(logZ))
+    print('probability: log=', logP, 'p=', exp(logP))
+
+
+def align_probability(i, j):
+    logf = forward(S1, S2, i, j)
+    logb = backward(S1, S2, i+1, j+1)
+    Z = forward(S1, S2, len(S1), len(S2))
+
+    print(logf, logb, Z)
+    # P = (fij exp(c/T) bi+1j+1) / fnm より
+    logP = logf + c(S1[i], S2[j])/T + logb - Z
+    return logP
 
 def argmax(List):
     return max(enumerate(List), key=lambda x: x[1])[0]
 
 
 
+score_probability()
 
-logZ = forward()
-# P = exp(S/T) / Z より、 logP = S/T - logZ
-Score = -1  # NWG.pyの実行結果
-logP = Score/T - logZ
-print('probability: log=', logP, 'p=', exp(logP))
+q1 = align_probability(0,3)
+q2 = align_probability(10,17)
+print('(i,j)=(0,3)', exp(q1))
+print('(i,j)=(10,17)', exp(q2))
